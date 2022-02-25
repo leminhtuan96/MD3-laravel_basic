fbb07acdfffe4983dbaf79b8f4ac944e52a742b0
leminhtuan96 <tuanmokc@gmail.com>
2022-02-25 20:31:05 +0700

    update task_manager

---
+++ task_manager/resources/views/modules/customer/index.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Danh sach khach hang</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th>STT</th>
+        <th>ho va ten</th>
+        <th>so dien thoai</th>
+        <th>Email</th>
+        <th>Thao tac</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>Nguyen Van A</td>
+        <td>01234567890</td>
+        <td>email.test@gmail.com</td>
+        <td>
+            <a href="#">xem</a> | <a href="#">Sua</a> | <a href="#">xoa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Nguyễn Văn B</td>
+        <td>01234567890</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Nguyễn Văn C</td>
+        <td>01234567890</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Nguyễn Văn D</td>
+        <td>01234567890</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Nguyễn Văn E</td>
+        <td>01234567890</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
